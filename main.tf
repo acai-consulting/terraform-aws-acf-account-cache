901abbaffcafac32ddc0326791f3d77e61698cf9
Michael Ullrich <michael.ullrich@nuvibit.com>
2024-05-30 08:36:03 +0200

    initial

--- main.tf
+++ main.tf
@@ -2,13 +2,12 @@
 # ¦ REQUIREMENTS
 # ---------------------------------------------------------------------------------------------------------------------
 terraform {
-  # This module is only being tested with Terraform 1.0 and newer.
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.3.10"
 
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = ">= 4.0"
+      version               = ">= 4.47"
       configuration_aliases = []
     }
   }
@@ -17,13 +16,208 @@ terraform {
 # ---------------------------------------------------------------------------------------------------------------------
 # ¦ DATA
 # ---------------------------------------------------------------------------------------------------------------------
-data "aws_caller_identity" "this" {}
+data "aws_caller_identity" "current" {}
 
 # ---------------------------------------------------------------------------------------------------------------------
 # ¦ LOCALS
 # ---------------------------------------------------------------------------------------------------------------------
-locals {}
+locals {
+  resource_tags = merge(
+    var.resource_tags,
+    {
+      "module_provider" = "ACAI GmbH",
+      "module_name"     = "terraform-aws-acf-account-cache",
+      "module_source"   = "github.com/acai-consulting/terraform-aws-acf-account-cache",
+      "module_version"  = /*inject_version_start*/ "1.0.0" /*inject_version_end*/
+    }
+  )
+}
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ¦ KMS CMK
+# ---------------------------------------------------------------------------------------------------------------------
+resource "aws_kms_key" "kms_cmk" {
+  description             = "This key will be used for encryption the policy repo, CloudWatch LogGroups, sns topics and the configure registry."
+  deletion_window_in_days = var.settings.kms_cmk.deletion_window_in_days
+  enable_key_rotation     = true
+  policy                  = data.aws_iam_policy_document.kms_cmk.json
+  tags                    = var.resource_tags
+}
+
+resource "aws_kms_alias" "kms_cmk" {
+  name          = var.settings.kms_cmk.alias_name
+  target_key_id = aws_kms_key.kms_cmk.key_id
+}
+
+#checkov:skip=CKV_AWS_111 : Resources cannot be defined, as KMS Key ARN is not known at creation time
+data "aws_iam_policy_document" "kms_cmk" {
+  #checkov:skip=CKV_AWS_356 : Resource policy
+  #checkov:skip=CKV_AWS_109
+  #checkov:skip=CKV_AWS_111
+  # enable IAM in logging account
+  override_policy_documents = var.settings.kms_cmk.policy_override
+  statement {
+    sid    = "ReadPermissions"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions = [
+      "kms:Describe*",
+      "kms:List*",
+      "kms:Get*",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "ManagementPermissions"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions = [
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:TagResource",
+      "kms:UntagResource",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "Allow Services"
+    effect = "Allow"
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+      "kms:CreateGrant"
+    ]
+    resources = ["*"]
+    condition {
+      test     = "StringLike"
+      variable = "kms:ViaService"
+      values = [
+        "dynamodb.*.amazonaws.com",
+        "ec2.*.amazonaws.com",
+        "elasticfilesystem.*.amazonaws.com",
+        "rds.*.amazonaws.com",
+        "redshift.*.amazonaws.com",
+        "backup.*.amazonaws.com"
+      ]
+    }
+  }
+  statement {
+    sid    = "Allow Lambda Execution Role"
+    effect = "Allow"
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+      "kms:CreateGrant"
+    ]
+    resources = ["*"]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "ArnLike"
+      variable = "aws:PrincipalARN"
+
+      values = [
+        replace("arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.settings.lambda_iam_role.path}${var.settings.lambda_iam_role.name}", "////", "/")
+      ]
+    }
+  }
+}
 
 # ---------------------------------------------------------------------------------------------------------------------
 # ¦ MAIN
 # ---------------------------------------------------------------------------------------------------------------------
+resource "aws_dynamodb_table" "context_cache" {
+  name = var.settings.ddb_name
+
+  hash_key     = "accountId"
+  range_key    = "contextId"
+  billing_mode = "PAY_PER_REQUEST"
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = aws_kms_key.kms_cmk.arn
+  }
+  ttl {
+    attribute_name = "timeToExist"
+    enabled        = true
+  }
+  attribute {
+    name = "accountId"
+    type = "S"
+  }
+  attribute {
+    name = "contextId"
+    type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = false
+  }
+
+  tags = var.resource_tags
+}
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ¦ LAMBDA
+# ---------------------------------------------------------------------------------------------------------------------
+module "lambda" {
+  #checkov:skip=CKV_TF_1: Currently version-tags are used
+  source  = "acai-consulting/lambda/aws"
+  version = "~> 1.3.2"
+
+  lambda_settings = {
+    function_name = var.settings.lambda_name
+    description   = "Query the cache."
+    layer_arn_list = [
+    ]
+    handler = "main.lambda_handler"
+    config  = var.lambda_settings
+    error_handling = var.lambda_settings.error_forwarder == null ? null : {
+      central_collector = var.lambda_settings.error_forwarder
+    }
+    package = {
+      source_path = "${path.module}/lambda-files"
+    }
+    tracing_mode = var.lambda_settings.tracing_mode
+    environment_variables = {
+      CONTEXT_CACHE_TABLE_NAME     = var.context_cache_name
+    }
+  }
+  execution_iam_role_settings = {
+    new_iam_role = {
+      name                     = var.lambda_settings.execution_role_name
+      path                     = var.semper_settings.core_security.general.iam_role_path
+      permissions_boundary_arn = var.semper_settings.core_security.general.iam_role_permissions_boundary_arn
+    }
+  }
+  existing_kms_cmk_arn = aws_kms_key.kms_cmk.arn
+  resource_tags        = local.resource_tags
+}
---
+++ variables.tf
@@ -0,0 +1,83 @@
+variable "settings" {
+  type = object({
+    lambda_name         = optional(string, "acai-account-cache")
+    lambda_iam_role = object({
+      name                     = string
+      path                     = optional(string, "/")
+      permissions_boundary_arn = optional(string, null)
+    })
+    ddb_name            = optional(string, "acai-account-cache")
+    kms_cmk = object({
+      alias_name              = string
+      deletion_window_in_days = optional(number, 30)
+      policy_override         = optional(list(string), null) # should override the statement_ids 'ReadPermissions' or 'ManagementPermissions'
+    })
+    kms_cmk_alias       = optional(string, "acai-account-cache-key")
+    org_reader_role_arn = string
+  })
+}
+
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ¦ LAMBDA SETTINGS
+# ---------------------------------------------------------------------------------------------------------------------
+variable "lambda_settings" {
+  description = "HCL map of the Lambda-Settings."
+  type = object({
+    architecture          = optional(string, "x86_64")
+    runtime               = optional(string, "python3.12")
+    log_level             = optional(string, "INFO") # Logging level, e.g. "INFO"
+    log_retention_in_days = optional(number, 7)      # Retention period for log files, in days
+    error_forwarder = optional(object({
+      target_name = string
+      target_arn  = string
+    }), null)
+    memory_size    = optional(number, 512) # Size of the memory, in MB
+    timeout        = optional(number, 720) # Timeout for the function, in seconds
+    tracing_mode   = optional(string, "Active")
+    crypted_source = optional(bool, true) # If true, source is encrypted
+  })
+
+  default = {
+    architecture          = "x86_64"
+    runtime               = "python3.12"
+    log_level             = "INFO"
+    log_retention_in_days = 7
+    error_forwarder       = null
+    memory_size           = 512
+    timeout               = 720
+    tracing_mode          = "Active"
+    crypted_source        = true
+  }
+
+  validation {
+    condition     = var.lambda_settings.architecture == null ? true : contains(["x86_64", "arm64"], var.lambda_settings.architecture)
+    error_message = "Architecture must be one of: \"x86_64\", \"arm64\", or null."
+  }
+
+  validation {
+    condition     = var.lambda_settings.log_level == null ? true : contains(["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], var.lambda_settings.log_level)
+    error_message = "log_level must be one of: \"DEBUG\", \"INFO\", \"WARNING\", \"ERROR\", \"CRITICAL\", or null."
+  }
+
+  validation {
+    condition     = var.lambda_settings.log_retention_in_days == null ? true : contains([0, 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 3653], var.lambda_settings.log_retention_in_days)
+    error_message = "log_retention_in_days value must be one of: 0, 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 3653, or null."
+  }
+
+  validation {
+    condition     = var.lambda_settings.tracing_mode == null ? true : contains(["PassThrough", "Active"], var.lambda_settings.tracing_mode)
+    error_message = "Value must be \"PassThrough\" or \"Active\"."
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ¦ VARIABLES
+# ---------------------------------------------------------------------------------------------------------------------
+variable "resource_tags" {
+  description = "Tags for the resources"
+  type        = map(string)
+  default     = {}
+}
