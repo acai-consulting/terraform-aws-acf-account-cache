2b52da377f1ac4abb55535256dcf13e2341fc36c
Michael Ullrich <michael.ullrich@acai.gmbh>
2024-06-21 13:52:54 +0200

    initial_version

--- cicd-principals/acf-cicd-manager/main.tf
+++ cicd-principals/acf-cicd-manager/main.tf
@@ -1,13 +1,12 @@
 data "template_file" "account_cache" {
   template = file("${path.module}/account_cache.yaml.tftpl")
   vars = {
-    lambda_name       = var.settings.lambda_name
-    lambda_layer_name = var.settings.lambda_layer_name
-    lambda_iam_role_name = var.settings.lambda_iam_role.name
-    lambda_iam_role_path = var.settings.lambda_iam_role.path
-    ddb_name          = var.settings.ddb_name
-    kms_cmk_alias = var.settings.kms_cmk.alias_name
-    api_key_name = var.settings.api_settings.api_key_name
-    api_name = var.settings.api_settings.api_key_name
+    lambda_name                = var.settings.lambda_name
+    lambda_layer_name          = var.settings.lambda_layer_name
+    lambda_exec_role_with_path = replace("/{var.settings.lambda_iam_role.path}/{var.settings.lambda_iam_role.name}", "//", "/")
+    policy_name                = replace(var.settings.lambda_iam_role.name, "role", "policy")
+    ddb_name                   = var.settings.ddb_name
+    api_key_name               = var.settings.api_settings.api_key_name
+    api_name                   = var.settings.api_settings.api_key_name
   }
 }
