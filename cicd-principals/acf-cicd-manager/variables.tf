1eda57f97fc026ce66649b401e84d561beb6fa0c
Michael Ullrich <michael.ullrich@acai.gmbh>
2024-06-21 15:22:56 +0200

    fix

--- cicd-principals/acf-cicd-manager/variables.tf
+++ cicd-principals/acf-cicd-manager/variables.tf
@@ -20,5 +20,21 @@ variable "settings" {
       api_name     = optional(string, "acai-cache")
     }), null)
   })
+  default = {
+    lambda_name       = "acai-account-cache"
+    lambda_layer_name = "acai-account-cache-layer"
+    lambda_iam_role = {
+      name = "acai-account-cache-lambda-role"
+      path = "/"
+    },
+    ddb_name = "acai-account-cache"
+    kms_cmk = {
+      alias_name = "alias/acai-account-cache-key"
+    },
+    api_settings = {
+      api_key_name = "acai-cache-key"
+      api_name     = "acai-cache"
+    }
+  }
 }
 
