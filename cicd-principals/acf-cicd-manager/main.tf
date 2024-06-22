e41f5f0ceda632e3aaa58a4840019e70a65cbb29
Michael Ullrich <michael.ullrich@acai.gmbh>
2024-06-22 11:31:28 +0200

    fix

--- cicd-principals/acf-cicd-manager/main.tf
+++ cicd-principals/acf-cicd-manager/main.tf
@@ -1,11 +1,10 @@
-data "template_file" "account_cache" {
-  template = file("${path.module}/account_cache.yaml.tftpl")
-  vars = {
+locals {
+  account_cache_rendered = templatefile("${path.module}//account_cache.yaml.tftpl", {
     lambda_name                = var.settings.lambda_name
     lambda_layer_name          = var.settings.lambda_layer_name
     lambda_exec_role_with_path = replace("/${var.settings.lambda_iam_role.path}/${var.settings.lambda_iam_role.name}", "////", "/")
     policy_name                = replace(var.settings.lambda_iam_role.name, "role", "policy")
     ddb_name                   = var.settings.ddb_name
-    include_api_gateway        = var.settings.api_settings != null    
-  }
+    include_api_gateway        = var.settings.api_settings != null
+  })
 }
