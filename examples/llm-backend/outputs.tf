c206deadcba719b4966a878db7ebe0a6f0d248d7
Michael Ullrich <michael.ullrich@acai.gmbh>
2024-06-24 08:54:29 +0200

    fix

--- examples/llm-backend/outputs.tf
+++ examples/llm-backend/outputs.tf
@@ -1,6 +1,6 @@
 output "example_passed" {
   description = "example_passed"
-  value       = contains(keys(jsondecode(data.aws_lambda_invocation.llm_backend.result)), "query")
+  value       = contains(keys(jsondecode(data.aws_lambda_invocation.llm_backend.result)["body"]), "query")
 }
 
 output "llm_backend" {
