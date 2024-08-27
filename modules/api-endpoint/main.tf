ab669ba1a25bf7388836cbe03272031c64eb197b
Michael Ullrich <micahel.ullrich@acai.gmbh>
2024-08-27 20:49:35 +0200

    fix

--- modules/api-endpoint/main.tf
+++ modules/api-endpoint/main.tf
@@ -184,8 +184,8 @@ resource "aws_api_gateway_integration" "options_integration" {
 }
 
 resource "aws_api_gateway_integration_response" "options_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
-  resource_id = aws_api_gateway_resource.MyDemoResource.id
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.cache_endpoint.id
   http_method = aws_api_gateway_method.options_integration.http_method
   status_code = aws_api_gateway_method_response.options_response_200.status_code
   response_parameters = {
