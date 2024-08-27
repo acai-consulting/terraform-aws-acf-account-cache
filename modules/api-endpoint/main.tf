6ad38db18244b466329da6a009f50136b846eef1
Michael Ullrich <micahel.ullrich@acai.gmbh>
2024-08-27 20:37:57 +0200

    adjust API

--- modules/api-endpoint/main.tf
+++ modules/api-endpoint/main.tf
@@ -153,3 +153,51 @@ resource "aws_api_gateway_integration_response" "cache_endpoint" {
     aws_api_gateway_integration.cache_endpoint
   ]
 }
+
+resource "aws_api_gateway_method" "options" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.cache_endpoint.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+
+}
+resource "aws_api_gateway_method_response" "options_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
+  resource_id = aws_api_gateway_resource.MyDemoResource.id
+  http_method = aws_api_gateway_method.options.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "options_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.cache_endpoint.id
+  http_method = aws_api_gateway_method.options.http_method
+  type        = "MOCK"
+}
+
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
+  resource_id = aws_api_gateway_resource.MyDemoResource.id
+  http_method = aws_api_gateway_method.options_integration.http_method
+  status_code = aws_api_gateway_method_response.options_response_200.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST'"
+  }
+  response_templates = {
+    "application/json" = ""
+  }
+  depends_on = [
+    aws_api_gateway_method.options,
+    aws_api_gateway_integration.options_integration
+  ]
+}
